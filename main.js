aa192d609c443a77aab6e2e80c31c1944813f0ef
Dario La Tegola <dariolategola@gmail.com>
2022-01-21 17:22:52 +0100

    Progetto completato

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const containerElement = document.getElementById("container");
 for (let i = 1; i <= 100; i++) {
 
     if (i%3 == 0 && i%5 == 0) {
-        containerElement.innerHTML = containerElement.innerHTML + "<div class='box'>FizzBuzz</div>"
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box fizzbuzz'>FizzBuzz</div>"
     }
     else if (i%3 == 0) {
-        containerElement.innerHTML = containerElement.innerHTML + "<div class='box'>Fizz</div>"
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box fizz'>Fizz</div>"
     }
     else if (i%5 == 0) {
-        containerElement.innerHTML = containerElement.innerHTML + "<div class='box'>Buzz</div>"
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box buzz'>Buzz</div>"
     }
     else {
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box'>" + i + "</div>"
